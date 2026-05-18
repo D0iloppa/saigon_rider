578fdfd7a9e6340f655804fbd341a0924d196eb4
D0iloppa <kdi3939@gmail.com>
2026-05-18 19:04:57 +0900

    sre-upgrade-in-progress

--- _tmp/sre-upgrade/design/item_catalog_v4_final/vite.config.ts
+++ _tmp/sre-upgrade/design/item_catalog_v4_final/vite.config.ts
@@ -8,7 +8,10 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         screens_v3_rpg: resolve(__dirname, 'screens_v3_rpg.html'),
+        item_catalog: resolve(__dirname, 'saigon-rider-item-catalog.html'),
       },
     },
+    assetsInlineLimit: 0,
   },
+  publicDir: 'public',
 })
